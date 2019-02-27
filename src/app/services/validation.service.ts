d1bb3ca5c5aaefc53cf64990e243d1b3680a78d7
Mevericks <reloved@gmail.com>
2019-02-27 14:03:21 +0200

    поправил правило при регистрации пользователя пароль 5 символов

--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -8,7 +8,7 @@ export class ValidationService {
             'required': 'Required',
             'invalidCreditCard': 'Is invalid credit card number',
             'invalidEmailAddress': 'Invalid email address',
-            'invalidPassword': 'Invalid password. Password must be at least 6 characters long, and contain a number.',
+            'invalidPassword': 'Invalid password. Password must be at least 5 characters long, and contain a number.',
             'minlength': `Minimum length ${validatorValue.requiredLength}`
         };
 
@@ -36,7 +36,7 @@ export class ValidationService {
     static passwordValidator(control) {
         // {6,100}           - Assert password is between 6 and 100 characters
         // (?=.*[0-9])       - Assert a string has at least one number
-        if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+        if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{5,100}$/)) {
             return null;
         } else {
             return { 'invalidPassword': true };
